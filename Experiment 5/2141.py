ee92d3ffc6483e773e0c3f70064aabb6d38771c9
Priyanka <163490585+Priyanka081104@users.noreply.github.com>
2026-04-24 23:17:59 +0530

    Update 2141.py

--- Experiment 5/2141.py
+++ Experiment 5/2141.py
@@ -1,1 +1,13 @@
+def maxRunTime(n, batteries):
+    batteries.sort()
+    total = sum(batteries)
 
+    left, right = 0, total // n
+    while left < right:
+        mid = (left + right + 1) // 2
+        if sum(min(b, mid) for b in batteries) >= n * mid:
+            left = mid
+        else:
+            right = mid - 1
+
+    return left
